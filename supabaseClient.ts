f7ace14452444dac19b95b7d5f76ceaa392ad4fe
zerobrand-team <zerobrandxyz@gmail.com>
2026-01-27 19:53:00 +0700

    Update supabaseClient.ts

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://kmxoigvmlhyahufmrnot.supabase.co' 
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtteG9pZ3ZtbGh5YWh1Zm1ybm90Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkzNjU0NzMsImV4cCI6MjA4NDk0MTQ3M30.j24TTFurhE7UOT1YRRLgmbci-0kzrvOd2bc04GKdWVA'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
