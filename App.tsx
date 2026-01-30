d312920e52098291b95d4cbf57458b6df074c00d
zerobrand-team <zerobrandxyz@gmail.com>
2026-01-31 01:41:53 +0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -382,7 +382,21 @@ const App: React.FC = () => {
                                         value={rawInput}
                                         onChange={(e) => setRawInput(e.target.value)}
                                     />
-                                </div>
+                                    <div className="flex justify-end mt-1">
+                                      <span
+                                        onClick={() => {
+                                          const sampleText = "I’m building a startup and constantly learning things the hard way.\nI want to share this on LinkedIn, but every time it sounds awkward or forced.\nNot sure how to turn these thoughts into a personal post.";
+                                          setRawInput(sampleText);
+                                          setTimeout(() => {
+                                            handleGenerate();
+                                          }, 0);
+                                        }}
+                                        className="text-[12px] text-gray-400 underline cursor-pointer hover:text-black transition-all"
+                                      >
+                                        Start with sample
+                                      </span>
+                                    </div>
+                                </div>
                                 
                                 <div className="flex items-start justify-between w-full mt-4">
                                     <div className="flex-shrink-0 ">
@@ -644,6 +658,20 @@ const App: React.FC = () => {
                                         value={rawInput}
                                         onChange={(e) => setRawInput(e.target.value)}
                                     />
+                                    <div className="flex justify-end mt-1">
+                                      <span
+                                        onClick={() => {
+                                          const sampleText = "I’m building a startup and constantly learning things the hard way.\nI want to share this on LinkedIn, but every time it sounds awkward or forced.\nNot sure how to turn these thoughts into a personal post.";
+                                          setRawInput(sampleText);
+                                          setTimeout(() => {
+                                            handleGenerate();
+                                          }, 0);
+                                        }}
+                                        className="text-[12px] text-gray-400 underline cursor-pointer hover:text-black transition-all"
+                                      >
+                                        Start with sample
+                                      </span>
+                                    </div>
                                 </div>
                                 
                                 <div className="flex items-start justify-between w-full mt-4">
